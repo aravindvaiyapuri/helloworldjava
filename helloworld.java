9d38d30c64d8f02268cc59252e34d2eff950a304
aravind <110608725+aravindvaiyapuri@users.noreply.github.com>
2022-08-23 14:18:43 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World at 2:01pm by araVIND.");
+    System.out.println("Hello World by webhook.");
     }
 }

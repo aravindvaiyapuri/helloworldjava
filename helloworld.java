ca889191b03061f562fafbc417422984d26a52ec
aravind <110608725+aravindvaiyapuri@users.noreply.github.com>
2022-08-23 14:32:20 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World by webhook.");
+    System.out.println("Hello World by webhook1.");
     }
 }
